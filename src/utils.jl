6afc4cda5815d6a359b9b8522cefb5d3fce06445
fetaxyu <fetaxyu@gmail.com>
2018-12-14 13:42:16 +0800

    function: list files

--- src/utils.jl
+++ src/utils.jl
@@ -3,3 +3,17 @@
 """
 is_odd(x::Int) = x & 0x1 == 1
 
+
+"""
+    list_files(dir::AbstractString=".")
+Return the files in the directory dir
+"""
+function list_files(dir::AbstractString=".")
+    rs = Vector{String}()
+    for (root, dirs, files) in walkdir(dir)
+        for file in files
+            push!(rs, joinpath(root, file))
+        end
+    end
+    rs
+end
