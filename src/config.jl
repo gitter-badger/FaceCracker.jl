d5ede843b1834d459e8d834447498a2c5b60700a
fetaxyu <fetaxyu@gmail.com>
2018-12-20 10:29:27 +0800

    global config

--- src/config.jl
+++ src/config.jl
@@ -1,5 +1,8 @@
+# global config
+
 const DATA_PATH = "../resources/datasets"
 const MODEL_PATH = "../resources/models"
 const INIT_LR = 0.003
+const CHANNELS = 3
 
-NUM_CLASSES = 10
+const NUM_CLASSES = 10
